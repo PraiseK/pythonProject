f3ab1334403aeba03db815a223455c501b28687f
Praise <praiserudairo.kamusasa@my.jcu.edu.au>
2022-06-14 16:10:44 +1000

    version 1.0

---
+++ cp1404_T2_pracs/week_03/lec_03/do_this_now_02.py
@@ -0,0 +1,22 @@
+def celsius_to_fahrenheit(celsius):
+    return celsius * 1.8 + 32.0
+    '''Convert celsius to fahrenheit'''
+
+
+def main():
+    number = 5
+    print(f'{number} ^ 2 = {square(number)}')
+    f_degree = celsius_to_fahrenheit(21)
+    # print(f'{f_degree:.1f}')
+
+
+# def is_even(number):
+#     return number % 2 == 0
+
+
+def square(number):
+    return number ** 2
+
+
+# square()
+main()
